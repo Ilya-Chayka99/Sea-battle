671af9936f52647f7e673d555114cd7b8dbc86ec
Ilya-Chayka99 <Ilya-Chayka99@yandex.ru>
2022-06-13 23:49:47 +0400

    По очереди, на рандом выбирается карта

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 let pol1=[]
 let pol2=[]
-//*******/Зануление двух полей/****************
-function zero()
-{
-    for(let i=0;i<100;i++)
-    {
-        pol1[i]=0;
-        pol2[i]=0;
-    }
-}
-zero()
+zero(0)
+//*******/Нажатие кнопок рандом/****************
+
+let b1=document.querySelector("#b1")
+let b2=document.querySelector("#b2")
+b1.addEventListener("click",()=>{ArrangementOfShips(1)})
+b2.addEventListener("click",()=>{ArrangementOfShips(2)})
+
+
 //*******/Печать полей/****************
 
 function PrintPole(num)
@@ -54,33 +53,31 @@ function pop(el)
 
 function ArrangementOfShips (num)
 {
+    zeroColor()
+    zero(num)
+
     if(num===1)
     {
         for (let j=4;j>0;j--)
         {
             let pol=Math.floor(Math.random()*2)
-            console.log(pol)
             if(pol===0)
             {
                 for(let kol=5-j;kol>0;kol--)
                 {
                     let stroka = Math.floor(Math.random()*10)
                     let stolbe = Math.floor(Math.random()*(10-j+1))
-                    console.log(stroka+" "+pol)
-                    console.log(stolbe+" "+pol)
                     if (pol1[stroka*10+stolbe]===0)
                     {
                         let sum=1;
                         for(let l=1;l<j;l++)
                             if(pol1[stroka*10+stolbe+l]===0) sum++
-                        console.log("aaaaaaaaaaaa="+sum+"  "+j+" "+kol+"   p="+pol)
                         if (sum!==j)
                         {
                             kol++
                         }
                         else
                         {
-                            console.log(pol1)
                             let pole1 = document.querySelectorAll(".kl1")
                             for(let i=0;i<j;i++)
                             {
@@ -101,21 +98,17 @@ function ArrangementOfShips (num)
                 {
                     let stroka = Math.floor(Math.random()*(10-j+1))
                     let stolbe = Math.floor(Math.random()*10)
-                    console.log(stroka+" "+pol)
-                    console.log(stolbe+" "+pol)
                     if (pol1[stroka*10+stolbe]===0)
                     {
                         let sum1=1;
                         for(let l=1;l<j;l++)
                             if(pol1[stroka*10+stolbe+l*10]===0) sum1++
-                        console.log("aaaaaaaaaaaa="+sum1+"  "+j+" "+kol)
                         if (sum1!==j)
                         {
                             kol++
                         }
                         else
                         {
-                            console.log(pol1)
                             let pole1 = document.querySelectorAll(".kl1")
                             for (let i = 0; i < j; i++) {
                                 pole1[stroka * 10 + stolbe + i * 10].style.backgroundColor = "blue"
@@ -129,35 +122,30 @@ function ArrangementOfShips (num)
 
             }
         }
-
+        render(1)
     }
     else
     {
         for (let j=4;j>0;j--)
         {
             let pol=Math.floor(Math.random()*2)
-            console.log(pol)
             if(pol===0)
             {
                 for(let kol=5-j;kol>0;kol--)
                 {
                     let stroka = Math.floor(Math.random()*10)
                     let stolbe = Math.floor(Math.random()*(10-j+1))
-                    console.log(stroka+" "+pol)
-                    console.log(stolbe+" "+pol)
                     if (pol2[stroka*10+stolbe]===0)
                     {
                         let sum=1;
                         for(let l=1;l<j;l++)
                             if(pol2[stroka*10+stolbe+l]===0) sum++
-                        console.log("aaaaaaaaaaaa="+sum+"  "+j+" "+kol+"   p="+pol)
                         if (sum!==j)
                         {
                             kol++
                         }
                         else
                         {
-                            console.log(pol1)
                             let pole2 = document.querySelectorAll(".kl2")
                             for(let i=0;i<j;i++)
                             {
@@ -178,21 +166,17 @@ function ArrangementOfShips (num)
                 {
                     let stroka = Math.floor(Math.random()*(10-j+1))
                     let stolbe = Math.floor(Math.random()*10)
-                    console.log(stroka+" "+pol)
-                    console.log(stolbe+" "+pol)
                     if (pol2[stroka*10+stolbe]===0)
                     {
                         let sum1=1;
                         for(let l=1;l<j;l++)
                             if(pol2[stroka*10+stolbe+l*10]===0) sum1++
-                        console.log("aaaaaaaaaaaa="+sum1+"  "+j+" "+kol)
                         if (sum1!==j)
                         {
                             kol++
                         }
                         else
                         {
-                            console.log(pol1)
                             let pole2 = document.querySelectorAll(".kl2")
                             for (let i = 0; i < j; i++) {
                                 pole2[stroka * 10 + stolbe + i * 10].style.backgroundColor = "blue"
@@ -206,12 +190,11 @@ function ArrangementOfShips (num)
 
             }
         }
+        render(2)
     }
-    render()
 }
 
-ArrangementOfShips(1)
-ArrangementOfShips(2)
+
 
 //*******/Зарисовка зоны вокруг кораблей/****************
 
@@ -231,7 +214,6 @@ function filling_the_space(kolP,polpj,koord,poles)
                 if (pol1[koord + i + 9] === 0 && (koord + i)%10 !== 0) pol1[koord + i + 9] = kolP * 10 + 1
                 if (pol1[koord + i + 10] === 0) pol1[koord + i + 10] = kolP * 10 + 1
                 if (pol1[koord + i + 11] === 0 && (koord + i)%10 !== 9) pol1[koord + i + 11] = kolP * 10 + 1
-                console.log((koord + i)%10 + "--------")
             }
         }
         else
@@ -277,7 +259,6 @@ function filling_the_space(kolP,polpj,koord,poles)
                 if (pol2[koord + i*10 + 9] === 0 && (koord + i*10)%10 !== 0) pol2[koord + i*10 + 9] = kolP * 10 + 1
                 if (pol2[koord + i*10 + 10] === 0 && (koord + i*10)/10 !== 9) pol2[koord + i*10 + 10] = kolP * 10 + 1
                 if (pol2[koord + i*10 + 11] === 0 && (koord + i*10)%10 !== 9) pol2[koord + i*10 + 11] = kolP * 10 + 1
-                console.log((koord + i)%10 + "--------")
             }
         }
     }
@@ -285,40 +266,91 @@ function filling_the_space(kolP,polpj,koord,poles)
 }
 
 
-function render()
+function render(num)
+{
+    if(num===1)
+    {
+        let pole1 = document.querySelectorAll(".kl1")
+        for(let i=0;i<100;i++)
+        {
+            let b=pol1[i]/10
+            let c=pol1[i]%10
+            if(b===0)
+            {
+
+            }
+            else
+            {
+                if(c===0) pole1[i].style.backgroundColor = "blue"
+                else pole1[i].style.backgroundColor = "green"
+            }
+
+        }
+    }
+
+    if(num===2)
+    {
+        let pole2 = document.querySelectorAll(".kl2")
+        for(let i=0;i<100;i++)
+        {
+            let b=pol2[i]/10
+            let c=pol2[i]%10
+            if(b===0)
+            {
+
+            }
+            else
+            {
+                if(c===0) pole2[i].style.backgroundColor = "blue"
+                else pole2[i].style.backgroundColor = "green"
+            }
+
+        }
+    }
+
+}
+
+
+//*******/Зануление двух полей/****************
+function zero(num)
+{
+    if(num===1)
+        for(let i=0;i<100;i++) pol1[i]=0;
+    if(num===2)
+        for(let i=0;i<100;i++) pol2[i]=0;
+    if(num===0)
+        for(let i=0;i<100;i++)
+        {
+            pol1[i]=0;
+            pol2[i]=0;
+        }
+}
+//*******/Окрашивание двух полей/****************
+function zeroColor()
 {
     let pole1 = document.querySelectorAll(".kl1")
-    for(let i=0;i<100;i++)
-    {
-        let b=pol1[i]/10
-        let c=pol1[i]%10
-        console.log(c)
-        if(b===0)
-        {
-
-        }
-        else
-        {
-            if(c===0) pole1[i].style.backgroundColor = "blue"
-             else pole1[i].style.backgroundColor = "green"
-        }
-
-    }
     let pole2 = document.querySelectorAll(".kl2")
+
     for(let i=0;i<100;i++)
     {
-        let b=pol2[i]/10
-        let c=pol2[i]%10
-        console.log(c)
-        if(b===0)
-        {
-
-        }
-        else
-        {
-            if(c===0) pole2[i].style.backgroundColor = "blue"
-             else pole2[i].style.backgroundColor = "green"
-        }
-
+        pole1[i].style.backgroundColor="white"
+        pole2[i].style.backgroundColor="white"
     }
+}
+
+//*******/Готовность 1го игрока/****************
+
+let pl1 = document.querySelector("#pl1")
+pl1.addEventListener("click",play1Ready)
+let pl2 = document.querySelector("#pl2")
+
+function play1Ready ()
+{
+    pl1.setAttribute("disabled","disabled")
+    b1.setAttribute("disabled","disabled")
+
+    pl2.removeAttribute("disabled")
+    b2.removeAttribute("disabled")
+    zeroColor()
+    console.log(pol1)
 }
